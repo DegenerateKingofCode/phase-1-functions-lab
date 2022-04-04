9123fe49a063a39789f410895ed19fb3deaabdf0
Tanner <Tanner@tleroy.com>
2022-04-04 18:32:03 -0400

    commit tree c929c2330360db2f459d90788e499ebe0484b17e

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    if (blockNumber >= 43)
+    return blockNumber - 42
+
+    else if (blockNumber <= 41)
+    return 42 - blockNumber
+}
+
+function distanceFromHqInFeet(blockNumber){
+    return distanceFromHqInBlocks(blockNumber) * 264
+}
+function distanceTravelledInFeet (start, destination){
+    if (start > destination)
+    return (start - destination) * 264
+
+    else if (start < destination)
+    return (destination - start) * 264
+}
+function calculatesFarePrice (start, destination){
+
+   if (distanceTravelledInFeet(start, destination) < 400)
+   return 0
+
+    else if (distanceTravelledInFeet(start, destination) > 400, distanceTravelledInFeet(start, destination)< 2000)
+    return (distanceTravelledInFeet(start, destination) - 400) * 0.02
+
+    else if (distanceTravelledInFeet(start, destination) >= 2000, distanceTravelledInFeet(start, destination) < 2500)
+    return 25
+
+    else if (distanceTravelledInFeet(start, destination) > 2500)
+    return "cannot travel that far"
+}
+    
